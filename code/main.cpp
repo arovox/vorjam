ef1006fccfbe3aa750223081747250add001db95
Alvaro <alvaro.daur@gmail.com>
2026-03-05 17:28:09 +0100

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,41 @@
+#include <RF24/RF24.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <ctime>
+
+RF24 radio(22, 0); // CE=GPIO22, CSN=CE0
+
+void initRadios() {
+    radio.begin();
+
+    radio.stopListening();
+    radio.setAutoAck(false);
+    radio.setRetries(0, 0);
+    radio.setPALevel(RF24_PA_MAX, true);
+    radio.setDataRate(RF24_2MBPS);
+    radio.setCRCLength(RF24_CRC_DISABLED);
+
+    // Startet den konstanten Träger wie beim ESP32
+    radio.startConstCarrier(RF24_PA_MAX, 45);
+}
+
+void hop() {
+    uint8_t ch = rand() % 81;
+    radio.setChannel(ch);
+
+    // kurze Pause wie beim ESP32
+    usleep((rand() % 60));  // Mikrosekunden
+}
+
+int main() {
+    srand(time(NULL));
+
+    initRadios();
+
+    while (true) {
+        hop();
+    }
+
+    return 0;
+}
+
